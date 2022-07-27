b80d82253ad81ab8d483d6a1eeb530b9b2030abb
chspower1 <chspower1@gmail.com>
2022-07-28 03:07:29 +0900

    first

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Seo from "../components/Seo";
+export default function Home() {
+    return (
+        <div>
+            <Seo title="About" />
+            <h1 className="active">about</h1>
+        </div>
+    );
+}
